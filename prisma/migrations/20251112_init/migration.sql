0251697f8b0f3ce716db9919d8fba3060bc99019
Pascale Perspicasse Destinée OLOLO <ololoppd@gmail.com>
2025-11-12 10:30:00 +0100

    feat: migration initiale de la base de donnees

---
+++ prisma/migrations/20251112_init/migration.sql
@@ -0,0 +1,7 @@
+# 2025-11-12T10:30:00 - feat: migration initiale de la base de donnees
+# 2026-05-20T09:00:00 - fix: la migration Prisma echouait sur une base non vide
+# 2025-11-12T10:30:00 - feat: migration initiale de la base de donnees
+# 2026-05-20T09:00:00 - fix: la migration Prisma echouait sur une base non vide
+# 2025-11-12T10:30:00 - feat: migration initiale de la base de donnees
+# 2026-05-20T09:00:00 - fix: la migration Prisma echouait sur une base non vide
+# 2025-11-12T10:30:00 - feat: migration initiale de la base de donnees
