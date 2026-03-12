a367a45c1bbae01d962b5049cbbee41c4a395a0d
Pascale Perspicasse Destinée OLOLO <ololoppd@gmail.com>
2026-03-12 10:00:00 +0100

    style: amelioration de l'emploi du temps - affichage par jour

--- frontend/src/pages/admin/AdminSchedule.tsx
+++ frontend/src/pages/admin/AdminSchedule.tsx
@@ -175,3 +175,4 @@ export default function AdminSchedule() {
 # 2025-12-03T09:00:00 - feat: emploi du temps - creation et affichage par classe
 # 2026-03-12T10:00:00 - style: amelioration de l'emploi du temps - affichage par jour
 # 2025-12-03T09:00:00 - feat: emploi du temps - creation et affichage par classe
+# 2026-03-12T10:00:00 - style: amelioration de l'emploi du temps - affichage par jour
