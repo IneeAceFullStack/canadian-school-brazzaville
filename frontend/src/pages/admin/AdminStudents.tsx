02b379f2bd856236f0a34cd0ff40df278c5a54f1
Pascale Perspicasse Destinée OLOLO <ololoppd@gmail.com>
2026-04-09 10:00:00 +0200

    fix: les dates s'affichaient en format ISO dans les tableaux

--- frontend/src/pages/admin/AdminStudents.tsx
+++ frontend/src/pages/admin/AdminStudents.tsx
@@ -201,3 +201,4 @@ export default function AdminStudents() {
 # 2026-02-04T11:00:00 - feat: pagination sur la liste des eleves
 # 2026-02-13T09:30:00 - chore: nettoyage des console.log et code mort
 # 2026-04-06T10:30:00 - style: responsive mobile - corrections sur les pages admin
+# 2026-04-09T10:00:00 - fix: les dates s'affichaient en format ISO dans les tableaux
