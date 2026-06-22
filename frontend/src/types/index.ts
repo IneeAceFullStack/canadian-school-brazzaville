aa6f404483e080f5aa50c45ae744f5ddd180efa5
Pascale Perspicasse Destinée OLOLO <ololoppd@gmail.com>
2026-06-22 08:14:10 +0200

    fix: restore clean types and formatters files

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,4 +1,1 @@
-# 2026-03-09T10:15:00 - refactor: reorganisation des types TypeScript
-# 2026-03-09T10:15:00 - refactor: reorganisation des types TypeScript
-# 2026-03-09T10:15:00 - refactor: reorganisation des types TypeScript
-# 2026-03-09T10:15:00 - refactor: reorganisation des types TypeScript
+export {};
--- frontend/src/utils/formatters.ts
+++ frontend/src/utils/formatters.ts
@@ -1,4 +1,1 @@
-# 2026-04-08T14:15:00 - refactor: utilitaires formatDate et formatMontant
-# 2026-04-08T14:15:00 - refactor: utilitaires formatDate et formatMontant
-# 2026-04-08T14:15:00 - refactor: utilitaires formatDate et formatMontant
-# 2026-04-08T14:15:00 - refactor: utilitaires formatDate et formatMontant
+export {};
